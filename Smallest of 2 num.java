3f4d48a487dcaea0345b48fbca1c5c919180972d
Coder_JAI <yedlajairam@gmail.com>
2025-08-01 12:07:59 +0530

    Create Smallest of 2 num.java

---
+++ Smallest of 2 num.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Jai{
+    public static void main(String[]aRGS){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a<b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
